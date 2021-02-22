f73e5c647a297c3c78db8214428604c958751c46
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-02-22 12:19:17 +0000

    refactored nested array to array of hashes

--- directory.rb
+++ directory.rb
@@ -1,26 +1,29 @@
 # students listed in an array variable
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex Delarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffery Baratheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex Delarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name:"Freddy Krueger", cohort: :november},
+  {name:"The Joker", cohort: :november},
+  {name:"Joffery Baratheon", cohort: :november},
+  {name:"Norman Bates", cohort: :november}
 ]
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
+
 def print(names)
   names.each do |student|
-    puts student
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
+
 def print_footer(names)
   puts "Overall we have #{names.count} great students"
 end

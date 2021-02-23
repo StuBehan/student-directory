6af3e405caf214ec843fdcf05aa6a0c6b001c9dc
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-02-23 16:04:07 +0000

    added method to print students with names beginning with specific letter

--- directory.rb
+++ directory.rb
@@ -7,10 +7,10 @@ students = [
   {name: "Alex Delarge", cohort: :november},
   {name: "The Wicked Witch of the West", cohort: :november},
   {name: "Terminator", cohort: :november},
-  {name:"Freddy Krueger", cohort: :november},
-  {name:"The Joker", cohort: :november},
-  {name:"Joffery Baratheon", cohort: :november},
-  {name:"Norman Bates", cohort: :november}
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffery Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 
 def print_header
@@ -47,8 +47,32 @@ def students_index(list)
   end
 end
 
+def begins_with(list)
+  puts "Specify a letter to search names with:"
+  letter = gets.chomp
+  while !letter.empty? do
+    counter = 0
+    while letter.length > 1
+      puts "Please enter only one character."
+      letter = gets.chomp
+    end
+    list.each do |student|
+      name = student[:name]
+      if name[0] == letter.upcase
+        counter = counter + 1
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+      end
+      if student == list[list.length - 1] && counter == 0
+        puts "There are no students who's name begins with #{letter.upcase}."
+      end
+    end
+    letter = gets.chomp
+  end 
+end
+
 # call methods
-students_index(students)
-print_header()
-print(students)
-print_footer(students)
+# students_index(students)
+# print_header()
+# print(students)
+# print_footer(students)
+begins_with(students)

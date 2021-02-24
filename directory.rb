218c986c9ed791bd8ba90178a4c054385cc53b46
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-02-24 15:47:17 +0000

    added makers_example.rb for reference, started to refactor input_student to reduce length

--- directory.rb
+++ directory.rb
@@ -1,17 +1,21 @@
 # students listed in an array of key value pairs 
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november, hobbies: "Music", weight: 76, height: 173},
-  {name: "Darth Vader", cohort: :november, hobbies: "Archeology", weight: 120, height: 203},
-  {name: "Nurse Ratched", cohort: :november, hobbies: "Religion", weight: 55, height: 168},
-  {name: "Michael Corleone", cohort: :november, hobbies: "Volunteer Work", weight: 57, height: 168},
-  {name: "Alex Delarge", cohort: :november, hobbies: "Community Service", weight: 56, height: 152},
-  {name: "The Wicked Witch of the West", cohort: :november, hobbies: "Flying", weight: 64, height: 168},
-  {name: "Terminator", cohort: :december, hobbies: "Travel", weight: 110, height: 183},
-  {name: "Freddy Krueger", cohort: :december, hobbies: "Gardening", weight: 73, height: 177},
-  {name: "The Joker", cohort: :december, hobbies: "Chiropterology", weight: 80, height: 182},
-  {name: "Joffery Baratheon", cohort: :december, hobbies: "Hunting", weight: 60, height: 172},
-  {name: "Norman Bates", cohort: :december, hobbies: "Taxidermy", weight: 82, height: 185}
-]
+# students = [
+#   {name: "Dr. Hannibal Lecter", cohort: :november, hobbies: "Music", weight: 76, height: 173},
+#   {name: "Darth Vader", cohort: :november, hobbies: "Archeology", weight: 120, height: 203},
+#   {name: "Nurse Ratched", cohort: :november, hobbies: "Religion", weight: 55, height: 168},
+#   {name: "Michael Corleone", cohort: :november, hobbies: "Volunteer Work", weight: 57, height: 168},
+#   {name: "Alex Delarge", cohort: :november, hobbies: "Community Service", weight: 56, height: 152},
+#   {name: "The Wicked Witch of the West", cohort: :november, hobbies: "Flying", weight: 64, height: 168},
+#   {name: "Terminator", cohort: :december, hobbies: "Travel", weight: 110, height: 183},
+#   {name: "Freddy Krueger", cohort: :december, hobbies: "Gardening", weight: 73, height: 177},
+#   {name: "The Joker", cohort: :december, hobbies: "Chiropterology", weight: 80, height: 182},
+#   {name: "Joffery Baratheon", cohort: :december, hobbies: "Hunting", weight: 60, height: 172},
+#   {name: "Norman Bates", cohort: :december, hobbies: "Taxidermy", weight: 82, height: 185}
+# ]
+
+
+@months = [ "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december" ]
+@students = []
 
 # prints header txt
 def print_header
@@ -26,19 +30,19 @@ end
 #   end
 # end
 
-# print rewritten to demonstrate loop control flow
-def print(list)
+# print rewritten to demonstrate while loop control flow
+def print_student_list
   count = 0
-  while count < list.length
-    puts "#{list[count].fetch(:name)} (#{list[count].fetch(:cohort)} cohort)".center(70)
+  while count < @students.count
+    puts "#{@students[count].fetch(:name)} (#{@students[count].fetch(:cohort)} cohort)".center(70)
     count += 1
   end
 end
 
 # prints footer text showing the total number of students in the given arguement 
-def print_footer(list)
+def print_footer
   puts "-------------".center(70)
-  puts "Overall we have #{list.count} great students".center(70)
+  puts "Overall we have #{@students.count} great students".center(70)
   puts "-------------".center(70)
 end
 
@@ -47,65 +51,18 @@ def input_students
   puts "To add students to the database you will require".center(70)
   puts "their name and specific cohort start month.".center(70)
   puts "To return to the menu, type \"back\" at the name entry stage.".center(70)
-  months = [ "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december" ] 
-  time = Time.new
-  name = ""
-  cohort = ""
-  students = []
-  cohort_correct = false
-  name_correct = false
+
 
   loop do
 
-    while name_correct == false
-      puts "Please enter the name of the student:".center(70) 
-      name = gets.chomp
-      case name
-      when "back"
-        break
-      when ""
-        puts "You can't leave the name field blank, try again".center(70)
-      else
-        puts "You entered \"#{name}\" is this correct? Y/N".center(70)
-        result = gets.chomp
-        if result.downcase == "y"
-          name_correct = true
-        else
-          name_correct = false
-        end
-      end
-    end
-    # breaking out of the nested loops
-    break if name == "back"
-
-    while cohort_correct == false
-      puts "Enter the month that #{name}'s cohort starts:".center(70)
-      cohort = gets.chomp
-      case cohort.downcase 
-      when ""
-        # this date make the default cohort start-date 2 months after the current month
-        month_number = time.month + 1
-        # wrap month around after december (months_number representing the index of the array months)
-        if month_number > 11
-          month_number -= 12
-        end
-        cohort = months[month_number]
-        cohort_correct = true
-      when "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"
-        cohort_correct = true
-      else
-        puts "You've entered the month incorrectly, you typed \"#{cohort}\",  try again.".center(70)
-      end
-    end
-
     while !name.empty? && !cohort.empty? do
-      students << {name: name, cohort: cohort.to_sym}
-      if students.count == 1
+      @students << {name: name, cohort: cohort.to_sym}
+      if @students.count == 1
         puts "Now we have 1 student!".center(70)
-        print(students)
+        print_student_list
       else
-        puts "Now we have #{students.count} students!".center(70)
-        print(students)
+        puts "Now we have #{@students.count} students!".center(70)
+        print_student_list
       end
       name = ""
       cohort = ""
@@ -113,12 +70,11 @@ def input_students
       cohort_correct = false
     end
   end
-  return students
 end
 
 # adds index starting at 1. for each entry
-def students_index(list)
-  list.each.with_index(1) do |value, index|
+def students_index
+  @students.each.with_index(1) do |value, index|
     puts "#{index}. #{value[:name]} (#{value[:cohort]} cohort)".center(70)
   end
 end
@@ -128,7 +84,7 @@ end
 
 # just thought I could have made the method create modified hash for 
 # each time it recieved a letter, deleting all entries which don't conform.
-def begins_with(list)
+def begins_with
   puts "Specify a letter to search names with:".center(70)
   letter = gets.chomp
   while !letter.empty? do
@@ -137,13 +93,13 @@ def begins_with(list)
       puts "Please enter only one character.".center(70)
       letter = gets.chomp
     end
-    list.each do |student|
+    @students.each do |student|
       name = student[:name]
       if name[0] == letter.upcase
         counter = counter + 1
         puts "#{student[:name]} (#{student[:cohort]} cohort)".center(70)
       end
-      if student == list[list.length - 1] && counter == 0
+      if student == @students[@students.length - 1] && counter == 0
         puts "There are no students who's name begins with #{letter.upcase}.".center(70)
       end
     end
@@ -152,9 +108,9 @@ def begins_with(list)
 end
 
 # prints only the names from the given arguement that are 11 characters long or fewer
-def short_names(list)
+def short_names
   list_new = []
-  list.each do |student|
+  @students.each do |student|
     if student[:name].length < 12
       list_new << student
     end
@@ -165,15 +121,15 @@ end
 
 # prints only the entered cohort's list of students
 # uses strip instead of chomp with the gets method, this cleans whitespace and LR at both ends of the string
-def print_cohort(list)
+def print_cohort
   puts "These are the current active cohorts:".center(70)
-  active_cohorts = list.uniq { |student| student[:cohort] }
+  active_cohorts = @students.uniq { |student| student[:cohort] }
   active_cohorts.each do |entry|
     puts "#{entry[:cohort]}".center(70)
   end
   puts "Enter a cohort start month to print students in that cohort".center(70)
   cohort_to_print = gets.strip.downcase.to_sym
-  single_cohort = list.select { |student| student[:cohort] == cohort_to_print }
+  single_cohort = @students.select { |student| student[:cohort] == cohort_to_print }
   single_cohort.each do |student| 
     puts "#{student[:name]} (#{student[:cohort]} cohort)".center(70)
   end
@@ -184,7 +140,6 @@ def interactive_menu
   puts "-------------".center(70)
   puts "Please make a selection from the menu".center(70)
   puts "-------------".center(70)
-  students = []
   loop do
     # print menu options
     puts "1. Input students".center(70)
@@ -195,15 +150,56 @@ def interactive_menu
     # implement selection
     case selection
     when "1"
-      students = input_students()
+      @students = input_students()
     when "2"
-      print_header()
-      print(students)
-      print_footer(students)
+      print_header
+      print_student_list
+      print_footer
+    when "3"
+      puts "#{@students[1]}"
     when "9"
       exit
     else
       puts "That selection is incorrect, try again.".center(70)
+    end
+  end
+end
+
+def enter_name
+  puts "Please enter the name of the student:".center(70) 
+  name = gets.chomp
+  while !name.empty?
+    puts "You entered \"#{name}\" is this correct? Y/N".center(70)
+    result = gets.chomp
+    if result.downcase == "y"
+      break
+    else
+      name = gets.chomp
+    end
+  end
+  return name
+end
+
+def enter_cohort
+  time = Time.new 
+  loop do
+    puts "Enter the month the cohort starts:".center(70)
+    cohort = gets.chomp.downcase
+    if cohort == ""
+      month_number = time.month + 1  # this date make the default cohort start-date 2 months after the current month
+      if month_number > 11
+        month_number -= 12  # wrap month around after december (months_number representing the index of the array months)
+      end
+      cohort = @months[month_number]
+      puts "Defaulted to the next cohort start date: #{cohort}.".center(70)
+      return cohort
+      break
+    end
+    if @months.include? cohort
+      return cohort
+      break
+    else
+      puts "You've entered the month incorrectly, you typed \"#{cohort}\",  try again.".center(70)
     end
   end
 end
@@ -217,4 +213,5 @@ end
 # begins_with(students)
 # short_names(students)
 # print_cohort(students)
-interactive_menu()
+# interactive_menu()
+enter_cohort

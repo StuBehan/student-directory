684c37d8d682436f056b48c6ec214bbef82f00de
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-02-25 17:52:18 +0000

    reordered methods for readability

--- directory.rb
+++ directory.rb
@@ -14,19 +14,128 @@
 # ]
 
 @months = [ "january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december" ]
+
 @students = []
+
+# runs the interactive menu in the loop
+def interactive_menu
+  loop do
+    print_menu
+    selection
+  end
+end
+
+# outputs the options for the interactive menu
+def print_menu
+  puts "Welcome to the Student Directory".center(70)
+  puts "Please make a selection from the menu".center(70)
+  puts "-------------".center(70)
+  puts "1. Input students".center(70)
+  puts "2. Show students".center(70)
+  puts "3. Save the student data to students.csv".center(70)
+  puts "4. Load the student data from students.csv".center(70)
+  puts "5. Select and show a specific cohorts students".center(70)
+  puts "6. Find students via the first letter of their name".center(70)
+  puts "9. Exit".center(70)
+end
+
+# runs the menu selections
+def selection
+  selection = STDIN.gets.chomp
+  case selection
+  when "1"
+    @students = input_students()
+  when "2"
+    print_header
+    print_student_list
+    print_footer
+  when "3"
+    save_students
+  when "4"
+    load_students
+  when "5"
+    print_cohort
+  when "6"
+    begins_with
+  when "9"
+    exit
+  else
+    puts "That selection is incorrect, try again.".center(70)
+  end
+end
+
+# allows user to enter a list of students names which are added to the november cohort
+def input_students
+  puts "To add students to the database you will require".center(70)
+  puts "their name and specific cohort start month.".center(70)
+  loop do
+    update_students(enter_name, enter_cohort)
+    if @students.count == 1
+      puts "Now we have 1 student!".center(70)
+    else
+      puts "Now we have #{@students.count} students!".center(70)
+    end
+    break if continue == false
+  end
+end
+
+# refactored from the input_students method for all name entries
+def enter_name
+  loop do
+    puts "Please enter the name of the student:".center(70) 
+    name = STDIN.gets.chomp
+    puts "You entered \"#{name}\" is this correct? Y/N".center(70)
+    result = STDIN.gets.chomp.downcase
+    if result == "y"
+      return name
+      break
+    end
+  end
+end
+
+# refactored from the input_students for cohort month entry and verification
+def enter_cohort
+  time = Time.new 
+  loop do
+    puts "Enter the month the cohort starts:".center(70)
+    cohort = STDIN.gets.chomp.downcase
+    if cohort == ""
+      month_number = time.month + 1  # this date make the default cohort start-date 2 months after the current month
+      if month_number > 11
+        month_number -= 12  # wrap month around after december (months_number representing the index of the array months)
+      end
+      cohort = @months[month_number]
+      puts "Defaulted to the next cohort start date: #{cohort}.".center(70)
+      return cohort
+      break
+    end
+    if @months.include? cohort
+      return cohort
+      break
+    else
+      puts "You've entered the month incorrectly, you typed \"#{cohort}\",  try again.".center(70)
+    end
+  end
+end
+
+# a breaker for exiting entry methods
+def continue
+  puts "Do you wish to add more? Y/N".center(70)
+  result = STDIN.gets.chomp.downcase
+  if result == "n"
+    return false
+  end
+end
+
+# removed the duplication of this code adding students to the array
+def update_students(name, cohort)
+  @students << {name: name, cohort: cohort.to_sym}
+end
 
 # prints header txt
 def print_header
   puts "The students of Villains Academy".center(70)
   puts "-------------".center(70)
-end
-
-# prints the key value pairs of the hash given as the arguement 
-def print_student_list
-  @students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(70)
-  end
 end
 
 # print rewritten to demonstrate while loop control flow
@@ -38,28 +147,17 @@ end
 #   end
 # end
 
+# prints the key value pairs of the hash given as the arguement 
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(70)
+  end
+end
+
 # prints footer text showing the total number of students in the given arguement 
 def print_footer
   puts "Overall we have #{@students.count} great students".center(70)
   puts "-------------".center(70)
-end
-
-# allows user to enter a list of students names which are added to the november cohort
-def input_students
-  puts "To add students to the database you will require".center(70)
-  puts "their name and specific cohort start month.".center(70)
-  puts "To return to the menu, type \"back\" at the name entry stage.".center(70)
-  loop do
-    update_students(enter_name, enter_cohort)
-    if @students.count == 1
-      puts "Now we have 1 student!".center(70)
-      print_student_list
-    else
-      puts "Now we have #{@students.count} students!".center(70)
-      print_student_list
-    end
-    break if continue == false
-  end
 end
 
 # adds index starting at 1. for each entry
@@ -124,95 +222,6 @@ def print_cohort
   end
 end
 
-# runs the interactive menu in the loop
-def interactive_menu
-  loop do
-    print_menu
-    selection
-  end
-end
-
-# outputs the options for the interactive menu
-def print_menu
-  puts "Welcome to the Student Directory".center(70)
-  puts "Please make a selection from the menu".center(70)
-  puts "-------------".center(70)
-  puts "1. Input students".center(70)
-  puts "2. Show students".center(70)
-  puts "3. Save the student data to students.csv".center(70)
-  puts "4. Load the student data from students.csv".center(70)
-  puts "9. Exit".center(70)
-end
-
-# shows the menu
-def selection
-  selection = STDIN.gets.chomp
-  case selection
-  when "1"
-    @students = input_students()
-  when "2"
-    print_header
-    print_student_list
-    print_footer
-  when "3"
-    save_students
-  when "4"
-    load_students
-  when "9"
-    exit
-  else
-    puts "That selection is incorrect, try again.".center(70)
-  end
-end
-
-# refactored from the input_students method for all name entries
-def enter_name
-  loop do
-    puts "Please enter the name of the student:".center(70) 
-    name = STDIN.gets.chomp
-    puts "You entered \"#{name}\" is this correct? Y/N".center(70)
-    result = STDIN.gets.chomp.downcase
-    if result == "y"
-      return name
-      break
-    end
-  end
-end
-
-# refactored from the input_students for cohort month entry and verification
-def enter_cohort
-  time = Time.new 
-  loop do
-    puts "Enter the month the cohort starts:".center(70)
-    cohort = STDIN.gets.chomp.downcase
-    if cohort == ""
-      month_number = time.month + 1  # this date make the default cohort start-date 2 months after the current month
-      if month_number > 11
-        month_number -= 12  # wrap month around after december (months_number representing the index of the array months)
-      end
-      cohort = @months[month_number]
-      puts "Defaulted to the next cohort start date: #{cohort}.".center(70)
-      return cohort
-      break
-    end
-    if @months.include? cohort
-      return cohort
-      break
-    else
-      puts "You've entered the month incorrectly, you typed \"#{cohort}\",  try again.".center(70)
-    end
-  end
-end
-
-# a breaker for exiting entry methods
-def continue
-  puts "Do you wish to add more? Y/N".center(70)
-  result = STDIN.gets.chomp.downcase
-  if result == "n"
-    return false
-  end
-end
-
 # saves the @students array to a CSV file
 def save_students
   file = File.open("students.csv", "w")
@@ -232,11 +241,6 @@ def load_students(filename = "students.csv")
   update_students(name, cohort)
   end
   file.close
-end
-
-# removed the duplication of this code adding students to the array
-def update_students(name, cohort)
-  @students << {name: name, cohort: cohort.to_sym}
 end
 
 # checks if filename exists and then pushes to load_students else quits

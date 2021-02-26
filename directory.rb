00fc8fe99faf0f53b98c420bb4df129f5095dae7
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-02-26 17:44:27 +0000

    refactor of load and save to use the File class

--- directory.rb
+++ directory.rb
@@ -232,13 +232,13 @@ def save_students(filename = get_filename)
   if filename.empty?
     filename = "students.csv"
   end
-  file = File.open(filename, "w")
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  File.open(filename, "w") do |file|
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
   end
-  file.close
 end
 
 # reusable filename entry
@@ -252,12 +252,12 @@ def load_students(filename = get_filename)
   if filename.empty?
     filename = "students.csv"
   end
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(',')
-  update_students(name, cohort)
+  File.open(filename, "r") do |file|
+    file.readlines.each do |line|
+      name, cohort = line.chomp.split(',')
+      update_students(name, cohort)
+    end
   end
-  file.close
 end
 
 # checks if filename exists and then pushes to load_students else quits
